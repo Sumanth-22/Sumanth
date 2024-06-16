178a314f5fb11af454708b9bf81c80751bd335fa
Sumanth-22 <107806497+Sumanth-22@users.noreply.github.com>
2024-06-16 10:37:07 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,90 @@
+public class Percentage
+{
+	public static void main(String[] args)
+		{
+			 String name = "Sumanth";
+  			String qual = "BTech";
+  			float s1=72,s2=92,s3=100,s4=89,s5=95,s6=98,per,total;
+  			total = s1+s2+s3+s4+s5+s6; 
+			per = (total/600)*100;
+			//per = 95;
+			System.out.println("my name is : "+name);
+ 			System.out.println("my qualification is : "+qual);
+ 			System.out.println("my total marks is : "+total);
+ 			System.out.println("my percentage is : "+per);
+                        if(per >50  && per <60)
+			System.out.print("obtained grade is : C  " + "PASS");
+			else if(per>=60 && per<70)
+			System.out.println("obtained grade is : B  " + "PASS");
+			else if(per>=70 && per<90)
+			System.out.println("obtained grade is : A  " + "PASS");
+			else if(per>=90)
+			System.out.println("obtained grade is : A+  " + "Distinction");
+			else
+			System.out.println("sorry u failed");
+ 			System.out.println("-----------------------------------------------------------------");
+			
+			String n="Dinesh";
+			String mailId ="dinesh.kothuri18@gmail.com";
+			String phone ="8142377077";
+			float sub1=34,sub2=33,sub3=32,sub4=31,sub5=30,sub6=29,perc,t;
+			t=sub1+sub2+sub3+sub4+sub5+sub6;
+			perc=(t/600)*100;
+			System.out.println("my name is : "+n);
+ 			System.out.println("my mailId is : "+mailId);
+			System.out.println("my phoneNumber is : "+phone);
+ 			System.out.println("my total marks is : "+t);
+ 			System.out.println("my percentage is : "+perc);
+                        if(perc >50  && perc <60)
+			System.out.print("obtained grade is : C  " + "PASS");
+			else if(perc>=60 && perc<70)
+			System.out.println("obtained grade is : B  " + "PASS");
+			else if(perc>=70 && per<90)
+			System.out.println("obtained grade is : A  " + "PASS");
+			else if(perc>=90)
+			System.out.print("obtained grade is : A+  " + "Distinction");
+			else
+			System.out.println("sorry u failed");
+
+			System.out.println("------------------------------------------------------------------------------");
+            	       		
+			String na="Kavya";
+			String mail="dinesh.kothuri18@gmail.com";
+			String ph="8142377077";
+			float su1=50,su2=50,su3=48,su4=35,su5=45,su6=7,percent,to;
+			to=su1+su2+su3+su4+su5+su6;
+			percent=(to/600)*100;
+			System.out.println("my name is : "+na);
+ 			System.out.println("my mailId is : "+mail);
+			System.out.println("my phoneNumber is : "+ph);
+ 			System.out.println("my total marks is : "+to);
+ 			System.out.println("my percentage is : "+percent);
+                        if(percent >50  && percent <60)
+			System.out.print("obtained grade is : C  " + "PASS");
+			else if(percent>=60 && percent<70)
+			System.out.println("obtained grade is : B  " + "PASS");
+			else if(percent>=70 && percent<90)
+			System.out.println("obtained grade is : A  " + "PASS");
+			else if(percent>=90)
+			System.out.print("obtained grade is : A+  " + "Distinction");
+			else
+			System.out.println("sorry u failed");
+
+			System.out.println("------------------------------------------------------------------------------");
+
+				
+			
+ 			
+
+			
+
+}
+}
+
+
+/*Percentage  Grade  O/p
+>50  <60     C    Obtained Grade C
+>=60  <70     B    Obtained Grade B
+>=70  <90     A    Obtained Grade A
+>=90          A+    Distinction*/
+
